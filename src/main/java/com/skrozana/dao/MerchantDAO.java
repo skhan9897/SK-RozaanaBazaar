66fdfe137a3c16e53507b1da3aba6967dd3af36b
Sajid Khan <msazidraza0786@gmail.com>
2026-09-18 20:18:21 +0530

    update

--- src/main/java/com/skrozana/dao/MerchantDAO.java
+++ src/main/java/com/skrozana/dao/MerchantDAO.java
@@ -6,6 +6,7 @@ import com.skrozana.util.DBConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
 
